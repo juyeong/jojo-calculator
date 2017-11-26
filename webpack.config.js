916e7a469417a727796ce8d41271b2c8c35e07ab
Tylor Shin <shincode@icloud.com>
2017-11-26 10:14:36 +0900

    Add basic typescript loader

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dst"),
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+};
