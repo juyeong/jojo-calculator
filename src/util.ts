b759358daef980a0ba73ecea139e6a38db472144
juyeong <dev@jy.is>
2018-12-16 15:46:09 +0900

    Add PWA GA events

--- src/util.ts
+++ src/util.ts
@@ -17,6 +17,22 @@ export function registerServiceWorker() {
         console.error('Service Worker Register Failed');
         console.error(e);
       });
+    window.addEventListener('beforeinstallprompt', async (e: any) => {
+      try {
+        ga('send', 'event', 'PWA', 'See', 'Add to homescreen');
+        const choiceResult = await e.userChoice;
+        const outcome = choiceResult.outcome;
+        const platform = choiceResult.platform;
+        console.log(outcome, platform);
+        if (outcome == 'dismissed') {
+          ga('send', 'event', 'PWA', 'Click', 'Dismiss');
+        } else {
+          ga('send', 'event', 'PWA', 'Click', 'Add');
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    });
   } else {
     console.log('serviceWorker not found in navigator');
   }
