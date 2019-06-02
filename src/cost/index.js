f0e27ad67208ca5798cbac62874808c604f1abe6
juyeong <dev@jy.is>
2019-06-02 18:20:40 +0900

    Update mongme

---
+++ src/cost/index.js
@@ -0,0 +1,244 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("babel-polyfill");
+require("./index.css");
+const util_1 = require("../util");
+const commanderData = require("./characters");
+const HAS_LOCAL_STORAGE = window.localStorage;
+function ready(fn) {
+    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+        fn();
+    }
+    else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+function main() {
+    let state = {
+        selectedCharacters: [],
+        totalCost: 0,
+    };
+    let inDefaultUpdate = false;
+    function getState() {
+        return state;
+    }
+    function setState(newState) {
+        return (state = Object.assign({}, getState(), newState));
+    }
+    function getCharacterInformation(id) {
+        return commanderData.find((commander) => id === commander.id);
+    }
+    function getTotalCost(commanders) {
+        return commanders.reduce((acc, val) => {
+            return acc + val.cost;
+        }, 0);
+    }
+    function getDisplayName(commander) {
+        return commander.desc == null ? commander.name : `${commander.name} ${commander.desc}`;
+    }
+    function getActiveCharacterNodes() {
+        return getState()
+            .selectedCharacters.map(char => {
+            return `<li class="list-group-item">
+          <div style="margin-bottom: 2px;">
+            <strong>${char.name}</strong> | ${char.class} | ${char.cost} cost
+          </div>
+          <div class=".text-dark" style="font-size: 0.9em;">
+            ${char.skill1} | ${char.skill2} | ${char.skill3} | ${char.skill4}
+          </div>
+        </li>`;
+        })
+            .join("");
+    }
+    function setStateAndRender(newState) {
+        setState({
+            selectedCharacters: newState,
+            totalCost: getTotalCost(newState),
+        });
+        renderCharacters();
+    }
+    function saveState() {
+        if (HAS_LOCAL_STORAGE) {
+            window.localStorage.setItem("ids", getState().selectedCharacters.map(commander => commander.id).join(","));
+        }
+    }
+    function loadState() {
+        if (HAS_LOCAL_STORAGE) {
+            let ids = window.localStorage.getItem("ids");
+            if (ids != null) {
+                return ids.split(",").map(id => parseInt(id));
+            }
+        }
+        return [491, 492, 493, 494, 495];
+    }
+    function getNewSaveId() {
+        if (HAS_LOCAL_STORAGE) {
+            const id = window.localStorage.getItem("_id");
+            let idNum = 1;
+            if (id != null) {
+                idNum = parseInt(id, 10) + 1;
+            }
+            window.localStorage.setItem("_id", `${idNum}`);
+            return idNum;
+        }
+    }
+    function saveCharacters(id, characters) {
+        if (characters.length == 0) {
+            return;
+        }
+        const ids = characters.map((c) => c.id).join(',');
+        window.localStorage.setItem(`saved_${id}`, `${ids}`);
+    }
+    function deleteCharacters(id) {
+        window.localStorage.removeItem(id);
+    }
+    function getSavedCharacterKeys() {
+        const localStorage = window.localStorage;
+        const keys = [];
+        for (let i = 0; i < localStorage.length; i++) {
+            const key = localStorage.key(i);
+            if (key.startsWith('saved_')) {
+                keys.push(key);
+            }
+        }
+        return keys.sort((key1, key2) => key1 < key2 ? 1 : -1);
+    }
+    function getSavedCharacter(key) {
+        const localStorage = window.localStorage;
+        return localStorage.getItem(key)
+            .split(',')
+            .map(id => parseInt(id, 10))
+            .map(id => getCharacterInformation(id));
+    }
+    function addNavListener() {
+        document.querySelectorAll(".nav-link")
+            .forEach((nav) => {
+            nav.addEventListener("click", () => {
+                const item = nav.getAttribute("data-item");
+                ga('send', 'event', 'Nav', 'Click', item);
+            });
+        });
+    }
+    function addSaveListener() {
+        document.querySelector('.commander-save').addEventListener('click', () => {
+            const savedKeys = getSavedCharacterKeys();
+            if (savedKeys.length >= 100) {
+                alert("최대 100개 까지 저장 가능");
+                return;
+            }
+            const saveId = getNewSaveId();
+            const characters = getState().selectedCharacters;
+            saveCharacters(saveId, characters);
+            renderSavedCharacters();
+        });
+    }
+    let select = $(".commander-input").selectize({
+        maxItems: 7,
+        hideSelected: true,
+        valueField: "id",
+        render: {
+            item: function (char) {
+                return `<div>${getDisplayName(char)}</div>`;
+            },
+            option: function (char) {
+                return `<div>${getDisplayName(char)}</div>`;
+            }
+        },
+        searchField: ["name", "desc", "aka"],
+        maxOptions: 9999,
+        sortField: {
+            field: "order",
+            direction: "desc"
+        },
+        options: commanderData,
+        create: false,
+        diacritics: true,
+        onItemAdd(value) {
+            const id = parseInt(value);
+            const char = getCharacterInformation(id);
+            const newState = getState().selectedCharacters.concat([char]);
+            setStateAndRender(newState);
+            if (!inDefaultUpdate) {
+                saveState();
+                ga('send', 'event', 'Character', 'Select', `${char.id}_${getDisplayName(char)}`);
+            }
+        },
+        onItemRemove(value) {
+            const id = parseInt(value);
+            const state = getState();
+            const index = state.selectedCharacters.findIndex(char => char.id === id);
+            if (index >= 0) {
+                const prevArr = state.selectedCharacters.slice(0, index);
+                const nextArr = state.selectedCharacters.slice(index + 1);
+                const newState = prevArr.concat(nextArr);
+                setStateAndRender(newState);
+                saveState();
+            }
+        },
+    });
+    function renderCharacters() {
+        if (inDefaultUpdate)
+            return;
+        const listParentNode = document.querySelector(".active-characters");
+        listParentNode.innerHTML = getActiveCharacterNodes();
+        const totalCostNode = document.querySelector(".total-cost-wrapper");
+        const totalCostValue = getState().totalCost;
+        let style;
+        if (totalCostValue > 145) {
+            style = "list-group-item-danger";
+        }
+        else if (totalCostValue > 99) {
+            style = "list-group-item-warning";
+        }
+        else {
+            style = "list-group-item-success";
+        }
+        totalCostNode.innerHTML = `
+    <div class="list-group-item list-group-item-action ${style}">
+    총 COST : ${totalCostValue}
+  </div>
+  `;
+    }
+    function renderSavedCharacters() {
+        const listParentNode = document.querySelector(".saved-characters");
+        const savedKeys = getSavedCharacterKeys();
+        listParentNode.innerHTML =
+            savedKeys.map((key) => {
+                let characters = getSavedCharacter(key);
+                return [key, characters.map(character => character.name).join(", "), characters.map(character => character.cost).reduce((acc, val) => val + acc, 0)];
+            })
+                .map((arr) => {
+                const key = arr[0];
+                const line = arr[1];
+                const cost = arr[2];
+                return `<li class="list-group-item" style="padding-bottom: 0;">[${cost}] ${line}<div style="display: flex; justify-content: flex-end">
+<!--<button type="button" class="btn btn-link">공유</button>-->
+<button type="button" class="saved-delete btn btn-link" data-key="${key}">삭제</button>
+</div>
+</li>`;
+            })
+                .join('');
+        const deleteButtons = document.querySelectorAll('.saved-delete');
+        for (let i = 0; i < deleteButtons.length; i++) {
+            const button = deleteButtons[i];
+            button.addEventListener('click', () => {
+                deleteCharacters(button.getAttribute('data-key'));
+                renderSavedCharacters();
+            });
+        }
+    }
+    function setDefaultItems() {
+        inDefaultUpdate = true;
+        select[0].selectize.setValue(loadState());
+        inDefaultUpdate = false;
+        renderCharacters();
+    }
+    window.setTimeout(() => setDefaultItems(), 0);
+    addSaveListener();
+    addNavListener();
+    window.setTimeout(() => renderSavedCharacters(), 0);
+    util_1.disableContextMenu();
+    util_1.registerServiceWorker();
+}
+ready(main);
+//# sourceMappingURL=index.js.map
--- src/mongme/index.ts
+++ src/mongme/index.ts
@@ -2,6 +2,7 @@ import {disableContextMenu, registerServiceWorker} from "../util";
 
 require("./mongme.css");
 const fg = require("fg-loadcss");
+const isBot = require("isBot");
 
 function ready(fn: (EventListenerOrEventListenerObject?: any, useCapture?: boolean) => void) {
   if ((document as any).attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
@@ -11,10 +12,7 @@ function ready(fn: (EventListenerOrEventListenerObject?: any, useCapture?: boole
   }
 }
 
-const GUIDE_LIST: IGuideLink[] = [
-  {source: "cafe", link: "https://cafe.naver.com/nexonjojo/621476", title: "논공행상 효율", author: "Berein"},
-  null,
-  {source: "dc", link: "https://gall.dcinside.com/board/view/?id=zohong&no=411084", title: "5월 몽매의 시련 동탁 완전 정리", author: "조랑보리"},
+const OLD_GUIDE_LIST: IGuideLink[] = [
   {source: "dc", link: "https://gall.dcinside.com/board/view/?id=zohong&no=415437", title: "내성 여는 조건", author: "조링조링"},
   {source: "dc", link: "https://gall.dcinside.com/board/view/?id=zohong&no=415684", title: "50만 몽매 출진장수 및 자리 참고용"},
   {source: "dc", link: "https://gall.dcinside.com/board/view/?id=zohong&no=415562", title: "[스압]몽매 동탁 개인 공략 1"},
@@ -25,9 +23,17 @@ const GUIDE_LIST: IGuideLink[] = [
   {source: "cafe", link: "https://cafe.naver.com/nexonjojo/625162", title: "(시간없는 분들을 위한) 동탁 25만딜 초간단 영상", author: "Reborn"},
 ];
 
+const GUIDE_LIST: IGuideLink[] = [
+  {source: "cafe", link: "https://cafe.naver.com/nexonjojo/621476", title: "논공행상 효율", author: "Berein"},
+  null,
+  {source: "dc", link: "https://gall.dcinside.com/board/view/?id=zohong&no=423299", title: "몽매의 시련 원술편 완전 가이드", author: "조랑보리"},
+  {source: "dc", link: "https://gall.dcinside.com/board/view/?id=zohong&no=433200", title: "조린이용 몽매 공략 (60만딜 보장)", author: "400만따리"},
+  {source: "dc", link: "https://gall.dcinside.com/board/view/?id=zohong&no=433047", title: "몽매의 시련 원술1편 허접한 공략(데이터)", author: "총퇴각"},
+  {source: "dc", link: "https://gall.dcinside.com/board/view/?id=zohong&no=433211", title: "몽매 공략 (조린이용)", author: "Yeoul"},
+];
+
 function loadCSS() {
-  fg.loadCSS(
-    "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css",
+  fg.loadCSS("https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css",
     null,
     null,
     {
@@ -37,40 +43,45 @@ function loadCSS() {
   );
 }
 
+function createListItem(guide: IGuideLink) {
+  const link = document.createElement("a");
+  link.href = guide.link;
+  link.target = "_href";
+  link.className = "list-group-item list-group-item-action";
+  const favicon = document.createElement("img");
+  if (guide.source === "cafe") {
+    favicon.src = "https://cafe.naver.com/favicon.ico";
+    favicon.alt = "공카";
+  } else if (guide.source === "dc") {
+    favicon.src = "https://gall.dcinside.com/favicon.ico";
+    favicon.alt = "조갤";
+  }
+  favicon.className = "guide-favicon";
+  const text = document.createElement("span");
+  text.innerText = guide.title;
+  text.className = "guide-title";
+  // const title = `${guide.title}${guide.author ? " by " + guide.author : ""}`;
+  link.appendChild(favicon);
+  link.appendChild(text);
+  return link;
+}
+
 function main() {
   registerServiceWorker();
   disableContextMenu();
-  loadCSS();
   const list = document.querySelector(".mongme-list");
   list.innerHTML = "";
-  GUIDE_LIST.forEach((guide) => {
+  const allItems = isBot(navigator.userAgent) ? [].concat(...GUIDE_LIST, ...OLD_GUIDE_LIST) : GUIDE_LIST;
+  allItems.forEach((guide: IGuideLink) => {
     if (guide) {
-      const link = document.createElement("a");
-      link.href = guide.link;
-      link.target = "_href";
-      link.className = "list-group-item list-group-item-action";
-      const favicon = document.createElement("img");
-      if (guide.source === "cafe") {
-        favicon.src = "https://cafe.naver.com/favicon.ico";
-        favicon.alt = "공카";
-      } else if (guide.source === "dc") {
-        favicon.src = "https://gall.dcinside.com/favicon.ico";
-        favicon.alt = "조갤";
-      }
-      favicon.className = "guide-favicon";
-      const text = document.createElement("span");
-      text.innerText = guide.title;
-      text.className = "guide-title";
-      // const title = `${guide.title}${guide.author ? " by " + guide.author : ""}`;
-      link.appendChild(favicon);
-      link.appendChild(text);
-      list.appendChild(link);
+      list.appendChild(createListItem(guide));
     } else {
       let li = document.createElement("li");
       li.className = "list-group-item";
       list.appendChild(li);
     }
   });
+  loadCSS();
 }
 
 interface IGuideLink {
