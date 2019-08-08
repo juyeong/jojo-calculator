87b713264fdd2663ec4b3ce59035b194762f7388
juyeong <dev@jy.is>
2019-08-08 16:55:13 +0900

    Update data

--- src/mongme/index.ts
+++ src/mongme/index.ts
@@ -21,16 +21,6 @@ const OLD_GUIDE_LIST: IGuideLink[] = [
   {source: "cafe", link: "https://cafe.naver.com/nexonjojo/624294", title: "조린이가 쓰는 몽매팁", author: "초오오고열용광로"},
   {source: "cafe", link: "https://cafe.naver.com/nexonjojo/624243", title: "무과금 슬로우 유저의 몽매의 시련 - 광기에 사로잡힌 동탁 편", author: "아이누족"},
   {source: "cafe", link: "https://cafe.naver.com/nexonjojo/625162", title: "(시간없는 분들을 위한) 동탁 25만딜 초간단 영상", author: "Reborn"},
-];
-
-const GUIDE_LIST: IGuideLink[] = [
-  {source: "cafe", link: "https://cafe.naver.com/nexonjojo/621476", title: "논공행상 효율", author: "Berein"},
-  null,
-  {source: "dc", link: "https://gall.dcinside.com/board/view/?id=zohong&no=439174", title: "몽매의 시련 안량편 (원소1) 불완전 가이드", author: "조랑보리"},
-  {source: "dc", link: "https://gall.dcinside.com/board/view/?id=zohong&no=439221", title: "몽매의 시련 문추편 (원소2) 불완전 가이드", author: "조랑보리"},
-  {source: "dc", link: "https://gall.dcinside.com/board/view/?id=zohong&no=439431", title: "몽매의 시련 원소편 (원소3) 완전 가이드", author: "조랑보리"},
-  null,
-  {source: "dc", link: "https://gall.dcinside.com/board/view/?id=zohong&no=423299", title: "몽매의 시련 원술편 완전 가이드", author: "조랑보리"},
   {source: "dc", link: "https://gall.dcinside.com/board/view/?id=zohong&no=433200", title: "조린이용 몽매 공략 (60만딜 보장)", author: "400만따리"},
   {source: "dc", link: "https://gall.dcinside.com/board/view/?id=zohong&no=433047", title: "몽매의 시련 원술1편 허접한 공략(데이터)", author: "총퇴각"},
   {source: "dc", link: "https://gall.dcinside.com/board/view/?id=zohong&no=433211", title: "몽매 공략 (조린이용)", author: "Yeoul"},
@@ -38,7 +28,14 @@ const GUIDE_LIST: IGuideLink[] = [
   {source: "cafe", link: "https://cafe.naver.com/nexonjojo/632122", title: "몽매-민폐황제 원술 낭창낭창하게 100만딜 만들기", author: "찰진엉덩국V"},
   {source: "cafe", link: "https://cafe.naver.com/nexonjojo/632154", title: "[몽매의 시련] 민폐 원술 - 167만 공략 (출진 순서 수정 + 영상 첨부)", author: "에이플러스"},
   {source: "cafe", link: "https://cafe.naver.com/nexonjojo/632250", title: "[아이누족] 무과금 슬로우 유저의 몽매의 시련 - 민폐 황제 원술 편 (131만)", author: "아이누족"},
-  {source: "cafe", link: "https://cafe.naver.com/nexonjojo/632704", title: "자부진인"},
+  {source: "cafe", link: "https://cafe.naver.com/nexonjojo/632704", title: "가짜 황제 원술 230만 공략 및 Tip.", author: "자부진인"},
+];
+
+const GUIDE_LIST: IGuideLink[] = [
+  {source: "cafe", link: "https://cafe.naver.com/nexonjojo/621476", title: "논공행상 효율", author: "Berein"},
+  null,
+  {source: "cafe", link: "https://cafe.naver.com/nexonjojo/642234", title: "100만딜 공략 기보", author: "Berein"},
+  {source: "dc", link: "https://gall.dcinside.com/mgallery/board/view/?id=johong&no=22229", title: "몽매 122만 공략", author: "400만따리"},
 ];
 
 function loadCSS() {
